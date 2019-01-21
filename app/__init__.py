290c7e2c6f3595b22aaeef26b06153ada241e6c8
Gichia <petergichia35@gmail.com>
2019-01-22 00:30:18 +0300

    [Finishes #163364695] Initiates a test db connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
-from .api.v2.utils.errors import bad_request, internal_server_error, not_found
-from .api.v2.views.user_views import ver2 as v2
-from .api.v2.views.meetup_views import ver2 as v2
+from app.api.utils.errors import bad_request, internal_server_error, not_found
+from app.api.views.user_views import ver2 as v2
+from app.api.views.meetup_views import ver2 as v2
+from instance.config import config
 
 
-def create_app(config):
-    '''function creating the flask app'''
-    app = Flask(__name__)
+def create_app(config_name):
+    """function creating the flask app"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config[config_name])
+    app.config.from_pyfile('config.py')
     app.register_blueprint(v2)
     app.register_error_handler(404, not_found)
     app.register_error_handler(405, bad_request)

2bab78aa87cade7764be32b0f37aa03a71218b48
Gichia <petergichia35@gmail.com>
2019-01-22 11:14:26 +0300

    Edited the database queries to implement correct dbs

--- app/database.py
+++ app/database.py
@@ -5,14 +5,17 @@ def db_tables():
         meetup_id serial PRIMARY KEY NOT NULL,
         created_by INT NOT NULL,
         location CHAR(50) NOT NULL,
-        topic CHAR(50) NULL,
-        images CHAR(50) NULL
+        topic CHAR(50) NOT NULL,
+        createdOn CHAR(50) NOT NULL,
+        images CHAR(50) NULL,
+        tags CHAR(150) NULL
         )"""
 
     tbl2 = """CREATE TABLE IF NOT EXISTS questions (
         question_id serial PRIMARY KEY NOT NULL,
         meetup_id INT NOT NULL,
         user_id INT NOT NULL,
+        createdOn CHAR(150) NOT NULL,
         title CHAR(100) NOT NULL,
         body CHAR(150) NOT NULL
         )"""
@@ -21,6 +24,7 @@ def db_tables():
         comment_id serial PRIMARY KEY NOT NULL,
         question_id INT NOT NULL,
         user_id INT NOT NULL,
+        createdOn CHAR(100) NOT NULL,
         comment CHAR(150) NOT NULL
         )"""
 
@@ -28,7 +32,8 @@ def db_tables():
         rsvp_id serial PRIMARY KEY NOT NULL,
         meetup_id INT NOT NULL,
         user_id INT NOT NULL,
-        status CHAR(10) NOT NULL
+        response CHAR(10) NOT NULL,
+        createdOn CHAR(100) NOT NULL
         )"""
 
     tbl5 = """CREATE TABLE IF NOT EXISTS users (
@@ -39,11 +44,21 @@ def db_tables():
         created_on CHAR(50) NOT NULL,
         is_admin BOOLEAN NOT NULL DEFAULT FALSE,
         password CHAR(150) NOT NULL,
-        username CHAR(20) NULL
+        username CHAR(20) NULL,
+        phoneNumber INT NULL
         )"""
 
-    tables = [tbl1, tbl2, tbl3, tbl4, tbl5]
+    tbl6 = """CREATE TABLE IF NOT EXISTS votes (
+        vote_id serial PRIMARY KEY NOT NULL,
+        user_id INT NOT NULL,
+        question_id INT NOT NULL,
+        createdOn CHAR(100) NOT NULL,
+        is_like BOOLEAN NOT NULL
+        )"""
+
+    tables = [tbl1, tbl2, tbl3, tbl4, tbl5, tbl6]
     return tables
+
 
 def drop_tables():
     """Function to drop all tables after tests"""
@@ -52,7 +67,8 @@ def drop_tables():
     tbl3 = """DROP TABLE IF EXISTS questions CASCADE"""
     tbl4 = """DROP TABLE IF EXISTS comments CASCADE"""
     tbl5 = """DROP TABLE IF EXISTS rsvp CASCADE"""
+    tbl6 = """DROP TABLE IF EXISTS votes CASCADE"""
 
-    tables = [tbl1, tbl2, tbl3, tbl4, tbl5]
+    tables = [tbl1, tbl2, tbl3, tbl4, tbl5, tbl6]
     return tables
     

edd08ba136187fdd5abd3b8eea83ea3333495c85
Gichia <petergichia35@gmail.com>
2019-01-22 19:44:56 +0300

    Added a more columns to the post meetup method

--- app/api/models/meetups_model.py
+++ app/api/models/meetups_model.py
@@ -1,19 +1,22 @@
 """Meetups Model to contain all meetup db operations"""
+import datetime
 from .basemodel import BaseModel
 
 class MeetupsClass(BaseModel):
     """Class to initiate db and contain common meetup methods"""
     
-    def post_meetup(self, user_id, location, topic, happeningOn):
+    def post_meetup(self, user_id, location, topic, happeningOn, tags):
         """Method to post a new meetup"""
         meetup = {
             "created_by": user_id,
             "location": location,
-            "topic": topic
+            "topic": topic,
+            "tags": tags,
+            "createdon": datetime.datetime.now()
         }
 
-        query = """INSERT INTO meetups (created_by, location, topic) 
-                VALUES ( %(created_by)s, %(location)s, %(topic)s )"""
+        query = """INSERT INTO meetups (created_by, location, topic, tags, createdon) 
+                VALUES ( %(created_by)s, %(location)s, %(topic)s, %(tags)s, %(createdon)s )"""
 
         data = self.post_data(query, meetup)
         return data

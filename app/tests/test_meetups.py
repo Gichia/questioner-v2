c6e4a167ebb9bea62c2ba61032d7c69974d32ea7
Gichia <petergichia35@gmail.com>
2019-01-23 15:52:47 +0300

    [Feature #163254521] Edits the test meetup test

--- app/tests/test_meetups.py
+++ app/tests/test_meetups.py
@@ -35,28 +35,6 @@ class TestMeetups(BaseTest):
         self.assertEqual(result["status"], 200)
         self.assertEqual(result2["message"], "Resource not found!")
 
-    def test_get_specific_meetup(self):
-        """Method to test get specific meetup endpoint"""
-        url = "http://localhost:5000/api/meetups/1"
-        url2 = "http://localhost:5000/api/meetups/0"
-        url3 = "http://localhost:5000/api/meetus/1"
-
-        self.post("http://localhost:5000/api/meetups", data)
-
-        response = self.get_items(url)
-        response2 = self.get_items(url2)
-        response3 = self.get_items(url3)
-
-        result = json.loads(response.data.decode("UTF-8"))
-        result2 = json.loads(response2.data.decode("UTF-8"))
-        result3 = json.loads(response3.data.decode("UTF-8"))
-
-        self.assertEqual(result["status"], 200)
-        self.assertEqual(result2["message"], "Meetup not found!")
-        self.assertEqual(result3["message"], "Resource not found!")
-
-        self.delete_meetup("Test Topic")
-
     def test_post_meetup(self):
         """Method to test post meetup endpoint"""
         url = "http://localhost:5000/api/meetups"
@@ -73,5 +51,29 @@ class TestMeetups(BaseTest):
         self.assertEqual(result["status"], 201)
         self.assertEqual(result2["message"], "Please provide all the required fields!")
         self.assertEqual(result3["status"], 401)
+
+
+    def test_get_specific_meetup(self):
+        """Method to test get specific meetup endpoint"""
+        query = """SELECT meetup_id FROM meetups WHERE topic=%s"""
+        topic = 'Test Topic'
+        self.curr.execute(query, (topic,))
+        tm = self.curr.fetchone()
+
+        url = "http://localhost:5000/api/meetups/{}".format(tm[0])
+        url2 = "http://localhost:5000/api/meetups/0"
+        url3 = "http://localhost:5000/api/meetus/1"
+
+        response = self.get_items(url)
+        response2 = self.get_items(url2)
+        response3 = self.get_items(url3)
+
+        result = json.loads(response.data.decode("UTF-8"))
+        result2 = json.loads(response2.data.decode("UTF-8"))
+        result3 = json.loads(response3.data.decode("UTF-8"))
+
+        self.assertEqual(result["status"], 200)
+        self.assertEqual(result2["message"], "Meetup not found!")
+        self.assertEqual(result3["message"], "Resource not found!")
 
         self.delete_meetup("Test Topic")

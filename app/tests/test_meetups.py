d30d1dfe4d40a5a90c768e50b64da1455e30e995
Gichia <petergichia35@gmail.com>
2019-01-23 12:13:00 +0300

    [Finishes #163254521] Adds unit tests for the endpoint

--- app/tests/test_meetups.py
+++ app/tests/test_meetups.py
@@ -9,7 +9,7 @@ class TestMeetups(BaseTest):
     """ Class to test all user endpoints """
 
     def test_get_meetups(self):
-        """Method to test user signup"""
+        """Method to test get all meetups endpoint"""
         url = "http://localhost:5000/api/meetups"
         url2 = "http://localhost:5000/api/meetup"
 
@@ -21,3 +21,21 @@ class TestMeetups(BaseTest):
 
         self.assertEqual(result["status"], 200)
         self.assertEqual(result2["message"], "Resource not found!")
+
+    def test_get_specific_meetup(self):
+        """Method to test get specific meetup endpoint"""
+        url = "http://localhost:5000/api/meetups/1"
+        url2 = "http://localhost:5000/api/meetups/100"
+        url3 = "http://localhost:5000/api/meetus/100"
+
+        response = self.get_items(url)
+        response2 = self.get_items(url2)
+        response3 = self.get_items(url3)
+
+        result = json.loads(response.data.decode("UTF-8"))
+        result2 = json.loads(response2.data.decode("UTF-8"))
+        result3 = json.loads(response3.data.decode("UTF-8"))
+
+        self.assertEqual(result["status"], 200)
+        self.assertEqual(result2["message"], "Meetup not found!")
+        self.assertEqual(result3["message"], "Resource not found!")

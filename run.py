5d194d73a90e3b7abbcd666ebfbcb6f77c0cd323
Gichia <petergichia35@gmail.com>
2019-01-30 10:54:32 +0300

    [Bug #163401424] Edits the run file

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 ''' Creates the base of the API'''
 from app import create_app
+from instance.config import config
 
 CONFIG_NAME = 'development'
 app = create_app(CONFIG_NAME)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

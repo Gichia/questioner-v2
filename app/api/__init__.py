4800a2fa5af2a44928a29867f193206c00dcfcb6
Gichia <petergichia35@gmail.com>
2019-01-30 09:30:55 +0300

    [Bug #163401424] Edits the blueprint for v2

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,4 @@
 """Initialize blueprint fro version2 of api"""
 from flask import Blueprint
 
-ver2 = Blueprint('ver2', __name__, url_prefix="/api")
+ver2 = Blueprint('ver2', __name__, url_prefix="/api/v2")

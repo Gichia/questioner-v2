458f9632a7202f3d83d14e22513462b0355bfa01
Gichia <petergichia35@gmail.com>
2019-01-30 09:40:28 +0300

    [Bug #163401424] Edits the test users file

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -8,7 +8,7 @@ from app.tests.basetest import BaseTest
 db_data = {
 	"firstname": "Kennedy",
 	"lastname": "Gichia",
-	"email": "ken2@gmail.com",
+	"email": "ken@gmail2.com",
 	"password": "Kennny@1",
 	"phone": "0706231221"
     }
@@ -39,12 +39,13 @@ class TestUsers(BaseTest):
         response = self.post(url, db_data)
         response2 = self.post(url, db_data2)
         response3 = self.post(url, db_data3)
+        
         result = json.loads(response.data.decode("UTF-8"))
         result_2 = json.loads(response2.data.decode("UTF-8"))
         result_3 = json.loads(response3.data.decode("UTF-8"))
 
         self.assertEqual(result["status"], 201)
-        self.assertEqual(result_2["message"], "Please provide correct details")
-        self.assertEqual(result_3["message"], "Please provide your lastname!")
+        self.assertEqual(result_2["error"], "Please provide all required details")
+        self.assertEqual(result_3["error"], "Please provide your lastname!")
 
-        self.delete_email("ken2@gmail.com")
+        self.delete_email("ken@gmail2.com")

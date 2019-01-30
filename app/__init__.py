f6e9da7854d25a89216d19bec67928a1aa47d95e
Gichia <petergichia35@gmail.com>
2019-01-30 10:51:55 +0300

    [Bug #163401424] Edits the connection file

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from app.api.utils.errors import bad_request, internal_server_error, not_found
 from app.api.views.user_views import ver2 as v2
 from app.api.views.meetup_views import ver2 as v2
 from app.api.views.question_views import ver2 as v2
-from app import db_conn
+from app.db_conn import InitDb
 from instance.config import config
 
 
@@ -12,9 +12,11 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config[config_name])
     app.config.from_pyfile('config.py')
+
     app.register_blueprint(v2)
     app.register_error_handler(404, not_found)
     app.register_error_handler(405, bad_request)
     app.register_error_handler(500, internal_server_error)
-    db_conn.create_tables()
+    InitDb(app)
+
     return app
